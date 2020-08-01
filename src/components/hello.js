379b0e31927a7d2abaeaf1f78ec6ac3649d92899
Vinesh <Vinesh.katewa@gmail.com>
2020-08-01 21:30:03 +0530

    Changes

--- src/components/hello.js
+++ src/components/hello.js
@@ -4,7 +4,7 @@ class Hello extends React.Component {
     render() {
         return (
             <div>
-                <h1>Are ya winning son?</h1>
+                <h1>Hello World</h1>
             </div>
         );
     }
