91518c26f382f3c0f7164239f0ba3db60dc9a4fe
Vinesh <Vinesh.katewa@gmail.com>
2020-09-01 20:32:31 +0530

    Re-arranged the folder structure

--- src/app.js
+++ src/app.js
@@ -1,25 +1,51 @@
 import React from 'react';
 import CodeEditor from './components/codeEditor.js';
 import ReactPrismEditor from "react-prism-editor";
-import Input from './Input';
-import Output from './Output'
-import './App.css';
+import Input from './components/Input.js';
+import Output from './components/Output.js'
+import './css/App.css';
 
 class App extends React.Component {
     render() {
         return (
-            <ReactPrismEditor
-                language={"c"}
-                theme={"default"}
-                code={""}
-                lineNumber={true}
-                readOnly={false}
-                clipboard={false}
-                changeCode={code => {
-                    this.code = code
-                    console.log(code)
-                }}
-            />
+            <>
+                <div className="container">
+                <div className="navbar">
+                    <img src="https://pbs.twimg.com/profile_images/927306982037643266/LNUHYNq7_400x400.png" alt="logo" className="logo" height="68px" width="75px" />
+                    <h1>CODE EDITOR</h1>
+                    <nav>
+                        <ul>
+                        <li><a href="#">HOME</a></li>
+                        <li><a href="#">ABOUT</a></li>
+                        <li><a href="#">CONTACT</a></li>
+                        </ul>
+                    </nav>
+                    </div>
+                    <div className="searchbox">search</div>
+                    <div className="templates">templates</div>
+                    <ReactPrismEditor
+                        className="input"
+                        language={"c"}
+                        theme={"default"}
+                        code={""}
+                        lineNumber={true}
+                        readOnly={false}
+                        clipboard={false}
+                        changeCode={code => {
+                            this.code = code
+                            console.log(code)
+                        }}
+                    />
+                    <div className="testcases">
+                        <Input />
+                    </div>
+                    <div className="output">
+                        <Output text="Something"/>
+                    </div>
+                    <div className="run">Run|Save|Save output</div>
+                    <div className="details">Execution Time and compile features</div>
+                </div>
+            </>
         );
     }
 }
