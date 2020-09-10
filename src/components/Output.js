fabf543b4105283e27782a9ba89efd0cca098ff3
DEEPALI-SUK <64724041+DEEPALI-SUK@users.noreply.github.com>
2020-09-10 23:15:19 +0530

    Update Output.js

--- src/components/Output.js
+++ src/components/Output.js
@@ -1,11 +1,41 @@
-import React from 'react';
+import React,{Component} from 'react';
+/*<div className="output">
+                        <Output text="Something"/>
+                    </div>
+                    <div className="run">
+                        <button className="brun">Run</button>
+                        <button className="bsave">Save</button>
+                         <button className="bsout">Save output</button>
+                    </div>
+                    <div className="details">Execution Time and compile features</div>*/
 
-function Output(props){
-    return(
-        <div>
-            <h4>Output</h4>
-            <div>{props.text}</div>
-        </div>
-    );
+class  Output extends Component{
+    render(){ 
+        return(
+          //Data.status
+          <div className="rightinside">
+          <div className="testcases">
+              <h3>Input</h3>
+              <input type="textarea" className="in"/>
+              </div>
+              <div className="output">
+                        <h3>Output</h3>
+                    </div>
+              <div className="run">
+                        <button className="brun">Run</button>
+                        <button className="bsave">Save</button>
+                         <button className="bsout">Save output</button>
+                    </div>
+                    <div className="details">
+                        <h4>Execution Time and compile features</h4>
+                    </div>
+
+              </div>
+          
+          
+      );
+      }
+  
 }
+
 export default Output;
